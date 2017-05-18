947ada7850a31c15a51c2389891c0eaf37cfe3ec
Thiago Figueira <tsantosfigueira@outlook.com>
2017-05-17 23:25:26 -0400

    version 0.1.0
    Added 'jump' capability to PlayerMovement script, allowing the player to
    jump given the jump height and input. Also corrected a problem allowing
    air double jump, which was not intended.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,33 +6,66 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     [Header("Player Movement Settings")]
-    public string horizontalAxis;    //!< Reference to player's horizontal axis movement
+    public string horizontalAxis = "Horizontal"; //!< Reference to player's horizontal axis movement
     [Range(1, 10)]
     public int movementSpeed;       //!< Player movement velocity
   //public string verticalAxis;     //!< Reference to player's vertical axis movement
     
 
     [Header ("Player Jump Settings")]
-    public string jumpAxis;          //!< Reference to player's jump movement button 
-    private bool isGrounded = true;  //!< Whether or not the player is grounded
+    public string jumpAxis = "Jump";  //!< Reference to player's jump movement button 
+    [Range(10, 100)]
+    public int jumpForce = 20;      //!< How high should the player jump
+    private Collider2D isGrounded;  //!< Whether or not the player is grounded
     public Transform groundCheck;    //!< A position marking where to check if the player is grounded
     public LayerMask groundLayer;    //!< Reference to the ground layer
 
-	
-	void FixedUpdate () {
+    private float maximumX;         //!< Maximum X position player can go
+    private float maximumY;         //!< Maximum Y position player can go
+
+    private bool jump;
+
+    public SpriteRenderer sprite;
+
+    void Start()
+    {
+        Vector3 cameraSize = Camera.main.ScreenToWorldPoint(new Vector3 (Camera.main.transform.position.x, 
+                                                                            Camera.main.transform.position.y,
+                                                                            Camera.main.transform.position.z));
+
+        maximumX = -cameraSize.x - sprite.size.x;
+        maximumY = -cameraSize.y - sprite.size.y;
+    }
+
+    void Update()
+    {
+        // Checks if player is grounded by drawing a circle around it and checking overlapping objects on ground layer
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.05f, groundLayer);
+        if (Input.GetButtonDown(jumpAxis) && isGrounded)
+            jump = true;
+    }
+
+    void FixedUpdate()
+    {
 
         float horizontal = Input.GetAxis(horizontalAxis);
-       // float vertical = Input.GetAxis(verticalAxis);
+        // float vertical = Input.GetAxis(verticalAxis);
 
         Vector2 movement = new Vector2(horizontal, 0);
         gameObject.GetComponent<Rigidbody2D>().velocity = movement * movementSpeed;
 
-        // Checks if player is grounded by drawing a circle around it and checking overlapping objects on ground layer
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.15f, groundLayer);
 
-        if (Input.GetButtonDown(jumpAxis) && isGrounded)
+        //transform.position.x = Mathf.Clamp(transform.position.x, -maximumX, maximumX);
+
+        gameObject.GetComponent<Rigidbody2D>().position = new Vector3(Mathf.Clamp(gameObject.GetComponent<Rigidbody2D>().position.x,
+                                                                       -maximumX, maximumX),
+
+                                                                      Mathf.Clamp(gameObject.GetComponent<Rigidbody2D>().position.y,
+                                                                      -maximumY, maximumY), 0);
+        if (jump)
         {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 20), ForceMode2D.Impulse);
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            jump = false;
         }
-	}
+    }
 }
